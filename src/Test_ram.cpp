585e09ece5a164e87e4bc76bd9fee69a7d88729b
esion-lin <bigesion@gmail.com>
2021-11-18 09:59:47 +0800

    update

--- src/Test_ram.cpp
+++ src/Test_ram.cpp
@@ -5,5 +5,8 @@ INITIALIZE_EASYLOGGINGPP
 int main(argc, char** argv){
     Config* cfg = new Config("./config.json");
     P2Pchannel* p2pchnl = new P2Pchannel(cfg->Pmap, argv[1]);
-
+    uint32_t arr[15] = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5};
+    Ram<uint32_t>* test_ram = Ram<uint32_t>(arr, 15, argv[1], p2pchnl);
+    test_ram->prepare_read(1);
+    std::cout<<test_ram->read(2)<<std::endl;
 }
